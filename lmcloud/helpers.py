8772570f14e7a736909874fa6bc90b9686cca452
Josef Zweck <24647999+zweckj@users.noreply.github.com>
2023-01-27 15:46:05 +0100

    change hour calc

--- lmcloud/helpers.py
+++ lmcloud/helpers.py
@@ -7,15 +7,14 @@ def convert_schedule(schedule):
     days = ["monday", "tuesday", "wednesday", "thursday", "friday", "saturday", "sunday"]
     schedule_conv = []
     for day in days: 
-        hh_mm_on = str(schedule[day]["h_on"]) + ":" + str(schedule[day]["m_on"]).zfill(2)
-        hh_mm_off = str(schedule[day]["h_off"]) + ":" + str(schedule[day]["m_off"]).zfill(2)
 
         # requests wants 00:00, response gives 24:00
-        if hh_mm_on == "24:00":
-            hh_mm_on = "00:00"
-        if hh_mm_off == "24:00":
-            hh_mm_off = "00:00"
-            
+        h_on = "00" if schedule[day]["h_on"] == 24 else str(schedule[day]["h_on"]).zfill(2)
+        h_off = "00" if schedule[day]["h_off"] == 24 else str(schedule[day]["h_off"]).zfill(2)
+
+        hh_mm_on = h_on + ":" + str(schedule[day]["m_on"]).zfill(2)
+        hh_mm_off = h_off + ":" + str(schedule[day]["m_off"]).zfill(2)
+
         schedule_conv.append({
             "day": str.upper(day),
             "enabled": schedule[day]["enabled"],
