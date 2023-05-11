a1facdd4ea53bf3df61ace450fbaa6dd03f1dd86
Josef Zweck <24647999+zweckj@users.noreply.github.com>
2023-05-11 09:13:22 +0200

    add correct characteristic, add temp setting

--- lmcloud/lmbluetooth.py
+++ lmcloud/lmbluetooth.py
@@ -1,5 +1,6 @@
 import asyncio
 from bleak import BleakScanner, BleakClient
+from .const import WRITE_SETTINGS_CHARACTERISTIC, MODEL_LMU
 
 class LMBluetooth:
     """
@@ -28,10 +29,17 @@ class LMBluetooth:
         devices = await scanner.discover()
         for d in devices:
             if d.name:
-                if  "MICRA" in d.name:
+                if MODEL_LMU.upper() in d.name:
                     self.address = d.address
                     self.name = d.name
 
+    
+    async def write_bluetooth_message(self, address: str, message: str):
+        async with BleakClient(self.address) as client:
+            if client.is_connected:
+                await client.write_gatt_char(WRITE_SETTINGS_CHARACTERISTIC, bytes(message, 'utf-8'))
+            else:
+                pass
 
     async def set_power(self, state: bool):
         """
@@ -39,19 +47,29 @@ class LMBluetooth:
         """
         mode = "BrewingMode" if state else "StandBy"
         msg = "{\"name\":\"MachineChangeMode\",\"parameter\":{\"mode\":\"" + mode + "\"}}"
-        async with BleakClient(self.address) as client:
-            if client.is_connected:
-                await client.write_gatt_char(TODO, bytes(msg, 'utf-8'))
-            else:
-                pass
+        self.write_bluetooth_message(self.address, msg)
 
     async def set_power(self, state: bool):
         """
         Power on steam
         """
         msg = "{\"name\":\"SettingBoilerEnable\",\"parameter\":{\"identifier\":\"SteamBoiler\",\"state\":" + state + "}}"
-        async with BleakClient(self.address) as client:
-            if client.is_connected:
-                await client.write_gatt_char(TODO, bytes(msg, 'utf-8'))
-            else:
-                pass
+        self.write_bluetooth_message(self.address, msg)
+
+    async def set_steam_temp(self, temperature:int):
+        '''
+        Set steamboiler temperature (in Celsius)
+        '''
+        if not temperature == 131 and not temperature == 128 and not temperature == 126:
+            msg = "Steam temp must be one of 126, 128, 131 (°C)"
+            raise ValueError(msg)
+        else:
+            msg = "{\"name\":\"SettingBoilerTarget\",\"parameter\":{\"identifier\":\"SteamBoiler\",\"value\":" +  str(temperature) + "}}"
+            self.write_bluetooth_message(self.address, msg)
+
+    async def set_coffee_temp(self, temperature:int):
+        '''
+        Set Cofeeboiler temperature (in Celsius)
+        '''
+        msg = "{\"name\":\"SettingBoilerTarget\",\"parameter\":{\"identifier\":\"CoffeeBoiler1\",\"value\":" +  str(temperature) + "}}"
+        self.write_bluetooth_message(self.address, msg)
