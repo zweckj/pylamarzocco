a603ed44f2a69506179b363fc8ee70c99d59b8bc
Josef Zweck <24647999+zweckj@users.noreply.github.com>
2024-01-14 22:31:40 +0100

    bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as f:
 
 setuptools.setup(
     name="lmcloud",
-    version="0.4.30",
+    version="0.4.31",
     description="A Python implementation of the new La Marzocco API",
     long_description=readme,
     long_description_content_type="text/markdown",
