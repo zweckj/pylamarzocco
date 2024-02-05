150835b64b96648c39af2e4d590f01bd00e5a101
Josef Zweck <24647999+zweckj@users.noreply.github.com>
2024-02-05 16:43:51 +0100

    cleanup BT client

--- lmcloud/client_bluetooth.py
+++ lmcloud/client_bluetooth.py
@@ -3,7 +3,9 @@
 from __future__ import annotations
 
 import base64
+import json
 import logging
+from typing import Any
 
 from bleak import BaseBleakScanner, BleakClient, BleakError, BleakScanner, BLEDevice
 
@@ -25,31 +27,23 @@ _logger = logging.getLogger(__name__)
 class LaMarzoccoBluetoothClient:
     """Class to interact with machine via Bluetooth."""
 
-    @property
-    def address(self) -> str:
-        """Return the BT MAC address of the machine."""
-        if self._address is None:
-            raise ClientNotInitialized("Bluetooth client not initialized")
-        return self._address
-
-    @property
-    def client(self) -> BleakClient | None:
-        """Return the Bluetooth client."""
-        return self._client
-
-    @property
-    def connected(self) -> bool:
-        """Return the connection status."""
-        if self._client is None:
-            return False
-        return self._client.is_connected
-
-    def __init__(self, username: str, serial_number: str, token: str) -> None:
+    def __init__(
+        self,
+        username: str,
+        serial_number: str,
+        token: str,
+        ble_device: BLEDevice | None = None,
+    ) -> None:
+        """Initializes a new LaMarzoccoBluetoothClient instance, optionally from a BLEDevice."""
         self._username = username
         self._serial_number = serial_number
         self._token = token
         self._address: str | None = None
         self._client: BleakClient | None = None
+
+        if ble_device:
+            self._address = ble_device.address
+            self._client = BleakClient(ble_device)
 
     @classmethod
     async def create(
@@ -64,9 +58,9 @@ class LaMarzoccoBluetoothClient:
         self = cls(username, serial_number, token)
         if bleak_scanner is None:
             async with BleakScanner() as scanner:
-                await self.discover_device(scanner)
+                await self._discover_device(scanner)
         else:
-            await self.discover_device(bleak_scanner)
+            await self._discover_device(bleak_scanner)
 
         if not self._address:
             # couldn't connect
@@ -76,21 +70,67 @@ class LaMarzoccoBluetoothClient:
             self._client = BleakClient(self._address)
         return self
 
-    @classmethod
-    def create_from_device(
-        cls,
-        username: str,
-        serial_number: str,
-        token: str,
-        ble_device: BLEDevice,
-    ) -> LaMarzoccoBluetoothClient:
-        """Init class with known device."""
-        self = cls(username, serial_number, token)
-        self._address = ble_device.address
-        self._client = BleakClient(ble_device)
-        return self
+    @property
+    def address(self) -> str:
+        """Return the BT MAC address of the machine."""
+        if self._address is None:
+            raise ClientNotInitialized("Bluetooth client not initialized")
+        return self._address
 
-    async def discover_device(self, scanner: BaseBleakScanner) -> None:
+    @property
+    def connected(self) -> bool:
+        """Return the connection status."""
+        if self._client is None:
+            return False
+        return self._client.is_connected
+
+    async def new_client_from_ble_device(self, ble_device: BLEDevice) -> None:
+        """Initalize a new bleak client from a BLEDevice (for Home Assistant)."""
+
+        self._client = BleakClient(ble_device)
+
+        try:
+            await self._client.connect()
+            await self._authenticate()
+        except (BleakError, TimeoutError) as e:
+            raise BluetoothConnectionFailed(
+                f"Failed to connect to machine with Bluetooth: {e}"
+            ) from e
+
+    async def set_power(self, state: bool) -> None:
+        """Power on the machine."""
+        mode = "BrewingMode" if state else "StandBy"
+        data = {
+            "name": "MachineChangeMode",
+            "parameter": {
+                "mode": mode,
+            },
+        }
+        await self._write_bluetooth_json_message(data)
+
+    async def set_steam(self, state: bool) -> None:
+        """Power cycle steam."""
+        data = {
+            "name": "SettingBoilerEnable",
+            "parameter": {
+                "identifier": "SteamBoiler",
+                "state": state,
+            },
+        }
+        await self._write_bluetooth_json_message(data)
+
+    async def set_temp(self, boiler: LaMarzoccoBoilerType, temperature: int) -> None:
+        """Set boiler temperature (in Celsius)"""
+        data = {
+            "name": "SettingBoilerTarget",
+            "parameter": {
+                "identifier": boiler,
+                "value": temperature,
+            },
+        }
+        await self._write_bluetooth_json_message(data)
+
+    async def _discover_device(self, scanner: BaseBleakScanner) -> None:
         """Find machine based on model name."""
         assert hasattr(scanner, "discover")
         devices = await scanner.discover()
@@ -100,7 +140,7 @@ class LaMarzoccoBluetoothClient:
                     self._address = d.address
 
     async def _write_bluetooth_message(
-        self, message: bytes | str, characteristic: str
+        self, characteristic: str, message: bytes | str
     ) -> None:
         """Connect to machine and write a message."""
         if self._client is None:
@@ -109,7 +149,7 @@ class LaMarzoccoBluetoothClient:
         if not self._client.is_connected:
             try:
                 await self._client.connect()
-                await self.authenticate()
+                await self._authenticate()
             except (BleakError, TimeoutError) as e:
                 raise BluetoothConnectionFailed(
                     f"Failed to connect to machine with Bluetooth: {e}"
@@ -127,49 +167,24 @@ class LaMarzoccoBluetoothClient:
 
         await self._client.write_gatt_char(characteristic, message)
 
-    async def authenticate(self) -> None:
+    async def _write_bluetooth_json_message(
+        self,
+        data: dict[str, Any],
+        characteristic: str = SETTINGS_CHARACTERISTIC,
+    ) -> None:
+        """Write a json message to the machine."""
+        await self._write_bluetooth_message(
+            characteristic=characteristic,
+            message=json.dumps(data, separators=(",", ":")),
+        )
+
+    async def _authenticate(self) -> None:
         """Build authentication string and send it to the machine."""
         user = self._username + ":" + self._serial_number
         user_bytes = user.encode("utf-8")
         token = self._token.encode("utf-8")
         auth_string = base64.b64encode(user_bytes) + b"@" + base64.b64encode(token)
-        await self._write_bluetooth_message(auth_string, AUTH_CHARACTERISTIC)
-
-    async def new_client_from_ble_device(self, ble_device: BLEDevice) -> None:
-        """Initalize a new bleak client from a BLEDevice (for Home Assistant)."""
-
-        self._client = BleakClient(ble_device)
-
-        try:
-            await self._client.connect()
-            await self.authenticate()
-        except (BleakError, TimeoutError) as e:
-            raise BluetoothConnectionFailed(
-                f"Failed to connect to machine with Bluetooth: {e}"
-            ) from e
-
-    async def set_power(self, state: bool) -> None:
-        """Power on the machine."""
-        mode = "BrewingMode" if state else "StandBy"
-        msg = '{"name":"MachineChangeMode","parameter":{"mode":"' + mode + '"}}'
-        await self._write_bluetooth_message(msg, SETTINGS_CHARACTERISTIC)
-
-    async def set_steam(self, state: bool) -> None:
-        """Power cycle steam."""
-        msg = (
-            '{"name":"SettingBoilerEnable","parameter":{"identifier":"SteamBoiler","state":'
-            + str(state).lower()
-            + "}}"
+        await self._write_bluetooth_message(
+            characteristic=AUTH_CHARACTERISTIC,
+            message=auth_string,
         )
-        await self._write_bluetooth_message(msg, SETTINGS_CHARACTERISTIC)
-
-    async def set_temp(self, boiler: LaMarzoccoBoilerType, temperature: int) -> None:
-        """Set boiler temperature (in Celsius)"""
-        msg = (
-            '{"name":"SettingBoilerTarget","parameter":{"identifier":"'
-            + str(boiler)
-            + '","value":'
-            + str(temperature)
-            + "}}"
-        )
-        await self._write_bluetooth_message(msg, SETTINGS_CHARACTERISTIC)
