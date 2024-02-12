651d28e022272807a4b03e4e8639da1c9d4dfe42
Josef Zweck <24647999+zweckj@users.noreply.github.com>
2024-02-12 10:19:40 +0100

    fix websocket parsing

--- tests/test_machine.py
+++ tests/test_machine.py
@@ -7,7 +7,7 @@ from syrupy import SnapshotAssertion
 
 from lmcloud.client_cloud import LaMarzoccoCloudClient
 from lmcloud.client_local import LaMarzoccoLocalClient
-from lmcloud.const import BoilerType, MachineModel, WeekDay
+from lmcloud.const import BoilerType, MachineModel, PhysicalKey, WeekDay
 from lmcloud.lm_machine import LaMarzoccoMachine
 
 from . import init_machine, MACHINE_SERIAL
@@ -71,12 +71,12 @@ async def test_set_prebrew_infusion(
             3.5,
         )
         assert result is True
-        assert machine.config.prebrew_configuration[1].on_time == 1.0
-        assert machine.config.prebrew_configuration[1].off_time == 3.5
+        assert machine.config.prebrew_configuration[PhysicalKey.A].on_time == 1.0
+        assert machine.config.prebrew_configuration[PhysicalKey.A].off_time == 3.5
 
         result = await machine.set_preinfusion_time(4.5)
         assert result is True
-        assert machine.config.prebrew_configuration[1].off_time == 4.5
+        assert machine.config.prebrew_configuration[PhysicalKey.A].off_time == 4.5
 
 
 async def test_set_schedule(
@@ -95,3 +95,22 @@ async def test_set_schedule(
             m_off=0,
         )
     assert result is True
+
+
+async def test_websocket_message(
+    cloud_client: LaMarzoccoCloudClient,
+    local_machine_client: LaMarzoccoLocalClient,
+    snapshot: SnapshotAssertion,
+):
+    """Test parsing of websocket messages."""
+    machine = await LaMarzoccoMachine.create(
+        model=MachineModel.GS3_AV,
+        serial_number=MACHINE_SERIAL,
+        name="MyMachine",
+        cloud_client=cloud_client,
+        local_client=local_machine_client,
+    )
+
+    message = r'[{"Boilers":"[{\"id\":\"SteamBoiler\",\"isEnabled\":true,\"target\":131,\"current\":113},{\"id\":\"CoffeeBoiler1\",\"isEnabled\":true,\"target\":94,\"current\":81}]"}]'
+    machine.on_websocket_message_received(message)
+    assert str(machine.config) == snapshot
