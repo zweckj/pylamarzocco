70ac766db72e8f260f02fcc33b8415f2f81287b9
Josef Zweck <24647999+zweckj@users.noreply.github.com>
2024-02-14 09:53:57 +0100

    pylint

--- tests/conftest.py
+++ tests/conftest.py
@@ -30,7 +30,7 @@ def load_fixture(device_type: str, file_name: str) -> dict:
         return json.load(f)
 
 
-def get_mock_response(*args, **kwargs) -> Response:  # pylint: disable=unused-argument
+def get_mock_response(*args, **kwargs) -> Response:
     """Get a mock response from HTTP request."""
     method: HTTPMethod = args[0]
     url: str = str(args[1])
