7cb552ec4b1026cc4bd7f159edd5a360445c77f9
Josef Zweck <josef@zweck.dev>
2025-03-30 18:38:36 +0000

    common base exception

--- pylamarzocco/exceptions.py
+++ pylamarzocco/exceptions.py
@@ -1,23 +1,27 @@
 """Exceptions for the pylamarzocco package."""
 
 
-class AuthFail(Exception):
+class LaMarzoccoError(Exception):
+    """Base exception for the La Marzocco package."""
+
+
+class AuthFail(LaMarzoccoError):
     """Error to indicate there is invalid auth info."""
 
 
-class RequestNotSuccessful(Exception):
+class RequestNotSuccessful(LaMarzoccoError):
     """Error to indicate didn't get a 200 response"""
 
 
-class BluetoothConnectionFailed(Exception):
+class BluetoothConnectionFailed(LaMarzoccoError):
     """Error to indicate we couldn't successfully connect through bluetooth"""
 
 
-class UnknownWebSocketMessage(Exception):
+class UnknownWebSocketMessage(LaMarzoccoError):
     """Error to indicate we received an unknown websocket message"""
 
 
-class CloudOnlyFunctionality(Exception):
+class CloudOnlyFunctionality(LaMarzoccoError):
     """Error to indicate the client is not initialized"""
 
     def __init__(self) -> None:
@@ -27,5 +31,5 @@ class CloudOnlyFunctionality(Exception):
         )
 
 
-class UnsupportedModel(Exception):
+class UnsupportedModel(LaMarzoccoError):
     """Raised when a certain functionality is only available on certain models."""
