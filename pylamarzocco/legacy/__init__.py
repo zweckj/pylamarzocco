efbd6477c1666467bdfe08bfdbb65d9d899f594c
Josef Zweck <josef@zweck.dev>
2025-04-21 17:41:06 +0000

    remove legacy code

--- pylamarzocco/legacy/__init__.py
+++
@@ -1,7 +0,0 @@
-"""Import for ease of use."""
-
-from pylamarzocco.legacy.clients.bluetooth import LaMarzoccoBluetoothClient
-from pylamarzocco.legacy.clients.cloud import LaMarzoccoCloudClient
-from pylamarzocco.legacy.clients.local import LaMarzoccoLocalClient
-from pylamarzocco.legacy.devices.grinder import LaMarzoccoGrinder
-from pylamarzocco.legacy.devices.machine import LaMarzoccoMachine
